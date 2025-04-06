30c38ed0efd8bee6433318588dc06b13d711f3db
KubaMackowski <jakobso2003@gmail.com>
2025-04-07 01:54:27 +0200

    fix: select input

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,7 +2,8 @@
 
 <select {{ $attributes->merge(['class' => 'mt-1 block w-full p-2 border rounded']) }}>
     @foreach($options as $option)
-        <option value="{{ $option->value }}" {{ $value == $option->value ? 'selected' : '' }}>
+        <option value="{{ $option->value }}"
+            {{ (is_string($value) ? $value : $value?->value) == $option->value ? 'selected' : '' }}>
             {{ ucfirst($option->value) }}
         </option>
     @endforeach
